ae13bffa229348118d3211dd053120ad4aea0c02
Julia <pythonoverjulia@gmail.com>
2022-09-05 20:08:34 +0100

    changes revoked

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,6 @@ var cors = require("cors");
 
 // let myDB = require("./data.json");
 let myDB = [];
-let entries = 0;
 router.use(cors());
 
 // let data = [];
@@ -53,47 +52,11 @@ router.post("/", function (req, res) {
   //   JSON.stringify(newEntry)
   myDB.push(newEntry); // req worked also for julika example JSON.stringify
   console.log("New entry added: ", myDB);
-
-  //   jsonReader("./users.json", (err, newEntry) => {
-  //     if (err) {
-  //       console.log("Error reading file:", err);
-  //       return;
-  //     }
-  //     // increase customer order count by 1
-  //     entries += 1;
-  //     fs.writeFile("./users.json", JSON.stringify(newEntry), (err) => {
-  //       if (err) console.log("Error writing file:", err);
-  //     });
-  //   });
 });
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  // alternative write proceudre:
   //res.send('respond with a resource');
-  //   var fs = require("fs");
-  //   fs.readFile("./users.json", function (err, data) {
-  //     var json = JSON.parse(data);
-  //     json.push(newEntry);
-  //     fs.writeFile("./users.json", JSON.stringify(json));
-  //   });
-  // reading from the JSON database
-  //   let entry;
-  //   const fs = require("fs");
-  //   fs.readFile("./users.json", "utf8", (err, req) => {
-  //     if (err) {
-  //       console.log("Error reading file from disk:", err);
-  //       return;
-  //     }
-  //     try {
-  //       entry = JSON.parse(req);
-  //       //   console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
-  //     } catch (err) {
-  //       console.log("Error parsing JSON string:", err);
-  //     }
-  //   });
-  //   res.json(entry);
-  // initially
   res.json(myDB);
 });
 
