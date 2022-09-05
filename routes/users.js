5fb26eabbc3b14a3db98926c4641e10776abd2a2
Julia <pythonoverjulia@gmail.com>
2022-09-05 18:32:50 +0100

    reverted back to old

--- routes/users.js
+++ routes/users.js
@@ -51,20 +51,20 @@ router.post("/", function (req, res) {
 
   console.log(newEntry);
   //   JSON.stringify(newEntry)
-  //   myDB.push(newEntry); // req worked also for julika example JSON.stringify
+  myDB.push(newEntry); // req worked also for julika example JSON.stringify
   console.log("New entry added: ", myDB);
 
-  jsonReader("./users.json", (err, newEntry) => {
-    if (err) {
-      console.log("Error reading file:", err);
-      return;
-    }
-    // increase customer order count by 1
-    entries += 1;
-    fs.writeFile("./users.json", JSON.stringify(newEntry), (err) => {
-      if (err) console.log("Error writing file:", err);
-    });
-  });
+  //   jsonReader("./users.json", (err, newEntry) => {
+  //     if (err) {
+  //       console.log("Error reading file:", err);
+  //       return;
+  //     }
+  //     // increase customer order count by 1
+  //     entries += 1;
+  //     fs.writeFile("./users.json", JSON.stringify(newEntry), (err) => {
+  //       if (err) console.log("Error writing file:", err);
+  //     });
+  //   });
 });
 
 /* GET users listing. */
@@ -72,34 +72,29 @@ router.get("/", function (req, res, next) {
   // alternative write proceudre:
   //res.send('respond with a resource');
   //   var fs = require("fs");
-
   //   fs.readFile("./users.json", function (err, data) {
   //     var json = JSON.parse(data);
   //     json.push(newEntry);
-
   //     fs.writeFile("./users.json", JSON.stringify(json));
   //   });
-
   // reading from the JSON database
-  let entry;
-  const fs = require("fs");
-  fs.readFile("./users.json", "utf8", (err, req) => {
-    if (err) {
-      console.log("Error reading file from disk:", err);
-      return;
-    }
-    try {
-      entry = JSON.parse(req);
-      //   console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
-    } catch (err) {
-      console.log("Error parsing JSON string:", err);
-    }
-  });
-
-  res.json(entry);
-
+  //   let entry;
+  //   const fs = require("fs");
+  //   fs.readFile("./users.json", "utf8", (err, req) => {
+  //     if (err) {
+  //       console.log("Error reading file from disk:", err);
+  //       return;
+  //     }
+  //     try {
+  //       entry = JSON.parse(req);
+  //       //   console.log("Customer address is:", customer.address); // => "Customer address is: Infinity Loop Drive"
+  //     } catch (err) {
+  //       console.log("Error parsing JSON string:", err);
+  //     }
+  //   });
+  //   res.json(entry);
   // initially
-  //   res.json(myDB);
+  res.json(myDB);
 });
 
 module.exports = router;
